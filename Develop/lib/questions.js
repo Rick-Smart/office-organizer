b46d97a2a3224486cc4d8f9faad58ca802d5e726
Xis <rick-s@hotmail.com>
2020-12-10 07:58:20 -0800

    added readme and screen shots

--- Develop/lib/questions.js
+++ Develop/lib/questions.js
@@ -1,41 +1,41 @@
 module.exports = [
-    {
-      type: "input",
-      name: "name",
-      message: "Employee Name",
-    },
-    {
-      type: "input",
-      message: "What is the employee id?",
-      name: "id",
-    },
-    {
-      type: "list",
-      message: "What is the employee role?",
-      name: "role",
-      choices: ["Engineer", "Intern"],
-    },
-    {
-        type: "input",
-        message: "Employee email?",
-        name: "email",
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "GitHub user name?",
-        when: (answers) => answers.role === "Engineer",
-    },
-    {
-        type: "input",
-        name: "school",
-        message: "School Name?",
-        when: (answers) => answers.role === "Intern",
-    },
-    {
-      type: "confirm",
-      name: "again",
-      message: "Enter another employee? ",
-      default: true,
-    },
-  ];
+  {
+    type: "input",
+    name: "name",
+    message: "Employee Name",
+  },
+  {
+    type: "input",
+    message: "What is the employee id?",
+    name: "id",
+  },
+  {
+    type: "list",
+    message: "What is the employee role?",
+    name: "role",
+    choices: ["Engineer", "Intern"],
+  },
+  {
+    type: "input",
+    message: "Employee email?",
+    name: "email",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "GitHub user name?",
+    when: (answers) => answers.role === "Engineer",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "School Name?",
+    when: (answers) => answers.role === "Intern",
+  },
+  {
+    type: "confirm",
+    name: "again",
+    message: "Enter another employee? ",
+    default: true,
+  },
+];
--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# office-organizer
+# Title: **Office Organizer**
+
+&nbsp;
+&nbsp;
+
+## Description:
+
+**This app allows you to add and remove employees and then display them to your screen with their own descriptive cards to be viewed in your browser**
+
+&nbsp;
+&nbsp;
+
+## Installation:
+
+_inquirer and jest are the 2 node packages you will need to install._
+
+&nbsp;
+&nbsp;
+
+## Contributors:
+
+**none**
+
+&nbsp;
+&nbsp;
+
+## Test:
+
+**open the app.js file in your terminal and follow the prompts.**
+
+&nbsp;
+&nbsp;
+
+## License:
+
+ISC
+
+&nbsp;
+&nbsp;
+
+## License URL:
+
+https://github.com/stevetrilogy/unit10hw/issues
+
+&nbsp;
+&nbsp;
+
+## GitHub UserName:
+
+@Rick-Smart
+
+&nbsp;
+&nbsp;
+
+## Email:
+
+**rick-s@hotmail.com**
+
+&nbsp;
+&nbsp;
+
+# Screen Shots
+
+&nbsp;
+&nbsp;
+![Screenshot (73)](https://user-images.githubusercontent.com/65750703/101795953-4ec9a580-3abd-11eb-9619-15d1dad94a17.png)
+&nbsp;
+&nbsp;
+![Screenshot (74)](https://user-images.githubusercontent.com/65750703/101795968-51c49600-3abd-11eb-8d51-d5e2963a3d0d.png)
+&nbsp;
+&nbsp;
+
+# Video Clip
+
+&nbsp;
+&nbsp;
+![](office-organizer.gif)
+&nbsp;
+&nbsp;
